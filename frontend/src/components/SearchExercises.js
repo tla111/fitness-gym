d899482309a75b812293a9d413147bcba49482c1
Tim La <tlamarketing111@gmail.com>
2022-11-29 15:22:54 -0500

    passed props in homepage

--- frontend/src/components/SearchExercises.js
+++ frontend/src/components/SearchExercises.js
@@ -5,9 +5,8 @@ import { exerciseOptions, fetchData } from '../utils/fetchData';
 
 import HorizontalScrollbar from './HorizontalScrollbar';
 
-const SearchExercises = () => {
+const SearchExercises = ({ setExercises, bodyPart, setBodyPart }) => {
     const [search, setSearch] = useState("")
-    const [exercises, setExercises] = useState([])
     const [bodyParts, setBodyParts] = useState([])
 
 
@@ -61,7 +60,7 @@ const SearchExercises = () => {
                 </Button>
             </Box>
             <Box sx={{ position: 'relative', width: '100%', p: '20px' }}>
-                <HorizontalScrollbar data={bodyParts} />
+                <HorizontalScrollbar data={bodyParts} bodyParts setBodyPart={setBodyPart} bodyPart={bodyPart} />
             </Box>
         </Stack>
     )
