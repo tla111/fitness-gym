1a8f238f90e0cf2f69d77a43559ee773b3785ada
Tim La <tlamarketing111@gmail.com>
2022-11-29 15:00:55 -0500

    able to fetch exercises api

--- frontend/src/utils/fetchData.js
+++ frontend/src/utils/fetchData.js
@@ -1,11 +1,10 @@
 export const exerciseOptions = {
     method: 'GET',
     headers: {
-        'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
-        'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com'
-    }
-
-}
+        'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com',
+        'X-RapidAPI-Key': '248d0d31e6msh3dbc20f058754e0p1d102cjsn3059651e6587'
+    },
+};
 
 export const fetchData = async (url, options) => {
     const res = await fetch(url, options);
