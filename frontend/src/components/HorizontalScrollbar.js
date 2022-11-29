251685055fb76096897798cc21b0f58f5cdd35be
Tim La <tlamarketing111@gmail.com>
2022-11-29 15:15:36 -0500

    created BodyPart component

--- frontend/src/components/HorizontalScrollbar.js
+++ frontend/src/components/HorizontalScrollbar.js
@@ -3,7 +3,7 @@ import { ScrollMenu, VisibilityContext } from 'react-horizontal-scrolling-menu';
 import { Box, Typography } from '@mui/material';
 
 // import ExerciseCard from './ExerciseCard';
-// import BodyPart from './BodyPart';
+import BodyPart from './BodyPart';
 // import RightArrowIcon from '../assets/icons/right-arrow.png';
 // import LeftArrowIcon from '../assets/icons/left-arrow.png';
 
