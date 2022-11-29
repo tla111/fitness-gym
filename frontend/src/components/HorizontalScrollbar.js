fbf177eead7704d1e0e480fad71d64efeeded8b4
Tim La <tlamarketing111@gmail.com>
2022-11-29 15:14:23 -0500

    able to retrieve bodyPart list

--- frontend/src/components/HorizontalScrollbar.js
+++ frontend/src/components/HorizontalScrollbar.js
@@ -2,15 +2,27 @@ import React, { useContext } from 'react';
 import { ScrollMenu, VisibilityContext } from 'react-horizontal-scrolling-menu';
 import { Box, Typography } from '@mui/material';
 
-import ExerciseCard from './ExerciseCard';
-import BodyPart from './BodyPart';
-import RightArrowIcon from '../assets/icons/right-arrow.png';
-import LeftArrowIcon from '../assets/icons/left-arrow.png';
+// import ExerciseCard from './ExerciseCard';
+// import BodyPart from './BodyPart';
+// import RightArrowIcon from '../assets/icons/right-arrow.png';
+// import LeftArrowIcon from '../assets/icons/left-arrow.png';
 
 
-const HorizontalScrollbar = () => {
+const HorizontalScrollbar = ({ data }) => {
     return (
-        <div>HorizontalScrollbar</div>
+        <div>
+            {data.map((item) => (
+                <Box
+                    key={item.id || item}
+                    itemId={item.id || item}
+                    title={item.id || item}
+                    m="0 40px"
+                >
+                    {item}
+                </Box>
+            )
+            )}
+        </div>
     )
 }
 
--- frontend/src/components/SearchExercises.js
+++ frontend/src/components/SearchExercises.js
@@ -3,6 +3,8 @@ import { Box, Button, Stack, TextField, Typography } from '@mui/material';
 
 import { exerciseOptions, fetchData } from '../utils/fetchData';
 
+import HorizontalScrollbar from './HorizontalScrollbar';
+
 const SearchExercises = () => {
     const [search, setSearch] = useState("")
     const [exercises, setExercises] = useState([])
