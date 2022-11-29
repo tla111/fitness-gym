dd23b726cb040e56e58a1484dd3162091db51e7d
Tim La <tlamarketing111@gmail.com>
2022-11-29 15:31:58 -0500

    bodypart typography is showing

--- frontend/src/components/BodyPart.js
+++ frontend/src/components/BodyPart.js
@@ -16,6 +16,7 @@ const BodyPart = ({ item, setBodyPart, bodyPart }) => {
         // }}
         >
             <img src={Icon} alt="dumbbell" style={{ width: '40px', height: '40px' }} />
+            <Typography fontSize="24px" fontWeight="bold" fontFamily="Alegreya" color="#3A1212" textTransform="capitalize"> {item}</Typography>
         </Stack>
     )
 }
