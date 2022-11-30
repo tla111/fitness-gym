e020c050fa254a5b2df737585adeefc6322e7d7b
Tim La <tlamarketing111@gmail.com>
2022-11-30 11:29:27 -0500

    saving work

--- frontend/src/components/SimilarExercises.js
+++ frontend/src/components/SimilarExercises.js
@@ -6,12 +6,12 @@ import HorizontalScrollbar from './HorizontalScrollbar';
 
 const SimilarExercises = () => {
     return (
-        <Box>
-            <Typography>
-
+        <Box sx={{ mt: { lg: '100px', xs: '0px' } }}>
+            <Typography sx={{ fontSize: { lg: '44px', xs: '25px' }, ml: '20px' }} fontWeight={700} color="#000" mb="33px">
+                Similar <span style={{ color: '#FF2625', textTransform: 'capitalize' }}>Target Muscle</span> exercises
             </Typography>
-            <Stack>
-
+            <Stack direction="row" sx={{ p: 2, position: 'relative' }}>
+                {targetMuscleExercises.length !== 0 ? <HorizontalScrollbar data={targetMuscleExercises} /> : <Loader />}
             </Stack>
         </Box>
     )
