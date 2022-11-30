8eed454c91e0fb2685fa12d0f42a1e2fead4addb
Tim La <tlamarketing111@gmail.com>
2022-11-30 10:50:09 -0500

    fixed number of exercises to be displayed

--- frontend/src/components/Exercises.js
+++ frontend/src/components/Exercises.js
@@ -7,7 +7,12 @@ import ExerciseCard from './ExerciseCard';
 
 const Exercises = ({ exercises, setExercises, bodyPart }) => {
     const [currentPage, setCurrentPage] = useState(1)
-    const exercisesPerPage = 9
+    const exercisesPerPage = 6
+
+    // Pagination
+    const indexOfLastExercise = currentPage * exercisesPerPage;
+    const indexOfFirstExercise = indexOfLastExercise - exercisesPerPage;
+    const currentExercises = exercises.slice(indexOfFirstExercise, indexOfLastExercise);
 
     const paginate = (event, value) => {
         setCurrentPage(value);
@@ -21,7 +26,7 @@ const Exercises = ({ exercises, setExercises, bodyPart }) => {
                 Showing Results
             </Typography>
             <Stack direction="row" sx={{ gap: { lg: '107px', xs: '50px' } }} flexWrap="wrap" justifyContent="center">
-                {exercises.map((exercise, idx) => (
+                {currentExercises.map((exercise, idx) => (
                     <ExerciseCard key={idx} exercise={exercise} />
                 ))}
             </Stack>
