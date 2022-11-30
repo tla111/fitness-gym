49cab3de676c1b13db8fab31fb4b05ca09dc8647
Tim La <tlamarketing111@gmail.com>
2022-11-30 10:58:34 -0500

    created components for ExerciseDetail page

--- frontend/src/pages/ExerciseDetail.js
+++ frontend/src/pages/ExerciseDetail.js
@@ -3,10 +3,17 @@ import { useParams } from 'react-router-dom';
 import { Box } from '@mui/material';
 
 import { exerciseOptions, fetchData, youtubeOptions } from '../utils/fetchData';
+import Detail from '../components/Detail';
+import ExerciseVideos from '../components/ExerciseVideos';
+import SimilarExercises from '../components/SimilarExercises';
 
 const ExerciseDetail = () => {
     return (
-        <div>ExerciseDetail</div>
+        <Box sx={{ mt: { lg: '96px', xs: '60px' } }}>
+            <Detail exerciseDetail={exerciseDetail} />
+            <ExerciseVideos exerciseVideos={exerciseVideos} name={exerciseDetail.name} />
+            <SimilarExercises targetMuscleExercises={targetMuscleExercises} equipmentExercises={equipmentExercises} />
+        </Box>
     )
 }
 
