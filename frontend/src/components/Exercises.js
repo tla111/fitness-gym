81ed3f2c3562569054d4b4c2e4cec696279cdc1a
Tim La <tlamarketing111@gmail.com>
2022-11-30 10:55:20 -0500

    able to differentiate between exercises on home page

--- frontend/src/components/Exercises.js
+++ frontend/src/components/Exercises.js
@@ -20,6 +20,22 @@ const Exercises = ({ exercises, setExercises, bodyPart }) => {
         window.scrollTo({ top: 1800, behavior: 'smooth' });
     };
 
+    useEffect(() => {
+        const fetchExercisesData = async () => {
+            let exercisesData = [];
+
+            if (bodyPart === 'all') {
+                exercisesData = await fetchData('https://exercisedb.p.rapidapi.com/exercises', exerciseOptions);
+            } else {
+                exercisesData = await fetchData(`https://exercisedb.p.rapidapi.com/exercises/bodyPart/${bodyPart}`, exerciseOptions);
+            }
+
+            setExercises(exercisesData);
+        };
+
+        fetchExercisesData();
+    }, [bodyPart]);
+
     return (
         <Box id="exercises" sx={{ mt: { lg: '109px' } }} mt="50px" p="20px">
             <Typography variant="h4" fontWeight="bold" sx={{ fontSize: { lg: '44px', xs: '30px' } }} mb="46px">
