e3b6c09368392636f4c21150965226b324858570
Tim La <tlamarketing111@gmail.com>
2022-11-30 11:14:25 -0500

    displaying title for videos

--- frontend/src/components/ExerciseVideos.js
+++ frontend/src/components/ExerciseVideos.js
@@ -1,8 +1,18 @@
-import React from 'react'
+import React from 'react';
+import { Typography, Box, Stack } from '@mui/material';
+// import Loader from './Loader';
 
-const ExerciseVideos = () => {
+
+const ExerciseVideos = ({ exerciseVideos, name }) => {
     return (
-        <div>ExerciseVideos</div>
+        <Box sx={{ marginTop: { lg: '203px', xs: '20px' } }} p="20px">
+            <Typography sx={{ fontSize: { lg: '44px', xs: '25px' } }} fontWeight={700} color="#000" mb="33px">
+                Watch <span style={{ color: '#FF2625', textTransform: 'capitalize' }}>{name}</span> exercise videos
+            </Typography>
+            {/* <Stack>
+
+            </Stack> */}
+        </Box>
     )
 }
 
