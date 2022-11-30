df8024cc47c5570b7cdec96f97ab41ae837189fa
Tim La <tlamarketing111@gmail.com>
2022-11-30 11:29:53 -0500

    passed props

--- frontend/src/components/SimilarExercises.js
+++ frontend/src/components/SimilarExercises.js
@@ -4,7 +4,7 @@ import { Typography, Box, Stack } from '@mui/material';
 import HorizontalScrollbar from './HorizontalScrollbar';
 
 
-const SimilarExercises = () => {
+const SimilarExercises = ({ targetMuscleExercises, equipmentExercises }) => {
     return (
         <Box sx={{ mt: { lg: '100px', xs: '0px' } }}>
             <Typography sx={{ fontSize: { lg: '44px', xs: '25px' }, ml: '20px' }} fontWeight={700} color="#000" mb="33px">
