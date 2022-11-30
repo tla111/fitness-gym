b312ab7f2ebf9923672f85ad9608588762c68cb4
Tim La <tlamarketing111@gmail.com>
2022-11-30 11:06:47 -0500

    displaying exercise name & description

--- frontend/src/components/Detail.js
+++ frontend/src/components/Detail.js
@@ -10,6 +10,17 @@ const Detail = ({ exerciseDetail }) => {
     return (
         <Stack gap="60px" sx={{ flexDirection: { lg: 'row' }, p: '20px', alignItems: 'center' }}>
             <img src={gifUrl} alt={name} loading="lazy" className="detail-image" />
+            <Stack sx={{ gap: { lg: '35px', xs: '20px' } }}>
+                <Typography sx={{ fontSize: { lg: '64px', xs: '30px' } }} fontWeight={700} textTransform="capitalize">
+                    {name}
+                </Typography>
+                <Typography sx={{ fontSize: { lg: '24px', xs: '18px' } }} color="#4F4C4C">
+                    Exercises keep you strong.{' '}
+                    <span style={{ textTransform: 'capitalize' }}>{name}</span> bup is one
+                    of the best <br /> exercises to target your {target}. It will help you improve your{' '}
+                    <br /> mood and gain energy.
+                </Typography>
+            </Stack>
         </Stack>
     )
 }
